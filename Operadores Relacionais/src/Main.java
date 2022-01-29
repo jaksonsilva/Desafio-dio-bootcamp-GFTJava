74ff9ba8cceb14f72fc94c72fb49a2f1ab3e2a26
jaksonsilva <jf.jaksonsilva@gmail.com>
2022-01-29 08:05:10 -0400

    Subindo mais uma pasta

---
+++ Operadores Relacionais/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void Main(String [] args){
+        if ((10 + 15) < 20)
+            System.out.println("Verdade");
+        else
+            System.out.println("Mentira");
+    }
+}
